623baea86382943d11cf7758ac1e1254f1561b0f
onuroezcelik <onur.oezcelik@gmail.com>
2025-12-02 12:34:07 +0000

    Update train.py

--- train.py
+++ train.py
@@ -55,13 +55,15 @@ def main():
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-    ds = ### YOUR CODE HERE ###
+    ds = TabularDatasetFactory.from_delimited_files(
+        "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    )
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
+    x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.3, random_state=50)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
